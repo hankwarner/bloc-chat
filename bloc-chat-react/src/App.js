3be3a009a1479e415b5eea94069b7e36121e72d8
hankwarner <40283259+hankwarner@users.noreply.github.com>
2018-08-07 19:04:17 -0400

    seeking assistance with checkpoint 2

--- bloc-chat-react/src/App.js
+++ bloc-chat-react/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList'; 
+
+https://www.gstatic.com/firebasejs/5.3.1/firebase.js
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyADnhsksV2J3_j6bKkx4Y2SaC9a_wmlIHo",
+    authDomain: "bloc-chat-9698d.firebaseapp.com",
+    databaseURL: "https://bloc-chat-9698d.firebaseio.com",
+    projectId: "bloc-chat-9698d",
+    storageBucket: "bloc-chat-9698d.appspot.com",
+    messagingSenderId: "578062214112"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+       <RoomList />
       </div>
     );
   }
